ce2790f49f35a0bbbe2ad820284390a88f2894af
Qistinatashah05 <167536197+Qistinatashah05@users.noreply.github.com>
2026-02-12 19:41:12 +0800

    login now okay

--- DatabaseConfig.java
+++ DatabaseConfig.java
@@ -19,6 +19,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DatabaseConfig {
     // 1. Ensure the DB name matches your DBeaver schema
     private static final String URL = "jdbc:mysql://localhost:3306/parking_management";
